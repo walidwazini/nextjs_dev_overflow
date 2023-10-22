448b04a79e59b2f1de55dafb79c824542106ea3e
Walid Wazini <wazini25@gmail.com>
2023-10-22 18:30:53 +0800

    mobileNav : create navContent (sidebar)

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -13,14 +13,34 @@ import {
 import Link from 'next/link'
 import Image from 'next/image'
 import { SignedOut } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
 
 import { Button } from '@/components/ui/button'
+import { sidebarLinks } from '@/constants'
 
 const NavContent = () => {
+  const pathname = usePathname()
+
   return (
-    <h1>
-      Nav Content
-    </h1>
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map(link => {
+        
+        const isActive = (pathname.includes(link.route) && link.route.length > 1 || pathname === link.route )
+
+        return (
+          <SheetClose asChild key={link.route} >
+            <Link href={link.route}
+            className={`${isActive ? 'primary-gradient rounded-lg text-light-900' : 'text-dark300_light900' } flex items-center justify-start gap-4 bg-transparent p-4 `}
+            >
+              <Image width={20} height={20} src={link.imgURL} alt={link.label}
+              className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? 'base-bold' : 'base-medium'}`} >{link.label}</p>
+            </Link>
+          </SheetClose>
+        )
+      })}
+    </section>
   )
 }
 
---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+
+
+
+export interface SidebarLink {
+  imgURL: string;
+  route: string;
+  label: string;
+}
