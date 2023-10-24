68ae6ca214f08f3f6c59d52d517b9f7ba138ce20
Walid Wazini <wazini25@gmail.com>
2023-10-24 08:44:02 +0800

    auth buttons on leftsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -7,6 +7,7 @@ import { usePathname } from 'next/navigation'
 import { SignedOut } from '@clerk/nextjs'
 
 import { sidebarLinks } from '@/constants'
+import { Button } from '../ui/button'
 
 const LeftSidebar = () => {
   const pathname = usePathname()
@@ -39,6 +40,36 @@ const LeftSidebar = () => {
           )
         })}
       </div>
+      <SignedOut>
+          <div className="flex flex-col gap-3">
+              <Link href="/sign-in">
+                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <Image 
+                    src="/assets/icons/account.svg"
+                    alt="login"
+                    width={20}
+                    height={20}
+                    className="invert-colors lg:hidden"
+                  /> 
+                  <span className="primary-text-gradient max-lg:hidden">Log In</span>
+                </Button>
+              </Link>
+
+          
+              <Link href="/sign-up">
+                <Button className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none'>
+                <Image 
+                    src="/assets/icons/sign-up.svg"
+                    alt="sign up"
+                    width={20}
+                    height={20}
+                    className="invert-colors lg:hidden"
+                  /> 
+                  <span className="max-lg:hidden">Sign up</span>
+                </Button>
+              </Link>
+          </div>
+        </SignedOut>
     </section>
   )
 }
