0e67c2b1d20859c1519a0f217482dcf0625c6e72
Walid Wazini <wazini25@gmail.com>
2023-10-30 13:14:52 +0800

    add multiple tag input

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import * as z from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Editor } from '@tinymce/tinymce-react'
+import Image from 'next/image'
 
 import { Button } from "@/components/ui/button"
 import {
@@ -13,19 +14,19 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { QuestionSchema } from '@/lib/validation'
+import { Badge } from '../ui/badge'
 
 const Question = () => {
   const editorRef = useRef()
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current);
-    }
-  };
+  const [tagDisable, setTagDisable] = useState(false)
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionSchema>>({
     resolver: zodResolver(QuestionSchema),
     defaultValues: {
       title: "",
+      explanation: "",
+      tags: [],
     },
   })
 
@@ -35,6 +36,41 @@ const Question = () => {
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
+
+
+  const handleInputKeyDown = (ev: React.KeyboardEvent<HTMLInputElement>, field: any) => {
+    if (ev.key === 'Enter' && field.name === 'tags') {
+      ev.preventDefault()
+
+      const tagInput = ev.target as HTMLInputElement
+      const tagValue = tagInput.value.trim()
+
+      if (tagValue !== '') {
+        if (tagValue.length > 15) {
+          return form.setError('tags', {
+            type: 'required',
+            message: 'Tag must be less than 15 characters.'
+          })
+        }
+
+
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue('tags', [...field.value, tagValue]);
+          tagInput.value = ''
+          form.clearErrors('tags');
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  }
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+
+    form.setValue('tags', newTags);
+  }
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex flex-col gap-10 " >
@@ -104,17 +140,39 @@ const Question = () => {
         {/* //? Tags  */}
         <FormField
           control={form.control}
-          name='explanation'
+          name='tags'
           render={({ field }) => (
             <FormItem className='flex w-full flex-col gap-3 ' >
               <FormLabel className='paragraph-semibold text-dark400_light800 ' >
                 Detail explanation of your problem <span className='text-primary-500' >*</span>
               </FormLabel>
               <FormControl className='mt-3.5' >
-                <Input
-                  className={`no-focus paragraph-regular background-light900_dark300 border-2  text-dark300_light700 min-h-[56px]`}
-                  placeholder='Add tags...'
-                />
+                <>
+                  <Input
+                    className={`no-focus paragraph-regular background-light900_dark300 border-2  text-dark300_light700 min-h-[56px]`}
+                    placeholder='Add tags...'
+                    onKeyDown={ev => handleInputKeyDown(ev, field)}
+                    disabled={field.value.length === 3}
+                  />
+                  {field.value?.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge key={tag} className={`subtle-medium background-light800_dark300 
+                      text-light400_light500 flex items-center justify-center gap-2 
+                      rounded-md border-none px-4 py-2 capitalize`}
+                          onClick={() => handleTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="Close icon" width={12} height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className='body-regular mt-2.5 text-light-500 ' >
                 Add up to 3 tags to describe your question. Press enter to add a tag.
--- lib/validation.ts
+++ lib/validation.ts
@@ -4,5 +4,5 @@ export const QuestionSchema = z.object({
   title: z.string().min(5).max(130),
   explanation: z.string().min(100),
   // Minimum 1 array, max 4 array.
-  tags: z.array(z.string().min(1).max(15)).min(1).max(4),
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
 })
