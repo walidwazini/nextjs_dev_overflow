bf097dbfc51058e84bbc6f91e35b4eaa14806d4c
Walid Wazini <wazini25@gmail.com>
2024-01-23 23:48:52 +0800

    handle url filter with filter component

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,24 +1,54 @@
 "use client"
 
+import React, { useState } from 'react'
 import { HomePageFilters } from '@/constants/filters'
-import React from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 import { Button } from '../ui/button'
+import { formUrlQuery } from '@/lib/utils'
 
 const HomeFilters = () => {
-  const isActive = 'frequent'
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  const [active, setActive] = useState('newest')
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'filter',
+        value: null
+      })
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'filter',
+        value: item.toLowerCase()
+      })
+
+      router.push(newUrl, { scroll: false });
+    }
+  }
 
   return (
     <div className='mt-10 hidden md:flex flex-wrap gap-3' >
       {HomePageFilters.map(item => (
-        <Button key={item.value} onClick={()=> {}}
-        className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none 
-        ${isActive === item.value 
-          ? 'bg-primary text-primary-500' 
-          : 'bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-200 dark:text-light-500 dark:hover:bg-dark-400 '}
+        <Button key={item.value}
+          onClick={() =>handleTypeClick(item.value)}
+          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none 
+        ${active === item.value
+              ? 'bg-primary text-primary-500'
+              : 'bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-200 dark:text-light-500 dark:hover:bg-dark-400 '}
           `}
         >
-            {item.name}
+          {item.name}
         </Button>
       ))}
     </div>
